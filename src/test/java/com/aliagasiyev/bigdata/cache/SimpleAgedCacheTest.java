d46fdd657580a5f1cfcc13becc1a4c56b3b44cc3
aliagasiyev <agasiyevali2@gmail.com>
2025-07-06 12:16:16 +0400

    feat(cache): add initial SimpleAgedCache class and basic tests
    - Implemented SimpleAgedCache skeleton with put/get/isEmpty/size methods
    - Added first JUnit test for empty cache on creation

--- src/test/java/com/aliagasiyev/bigdata/cache/SimpleAgedCacheTest.java
+++ src/test/java/com/aliagasiyev/bigdata/cache/SimpleAgedCacheTest.java
@@ -11,4 +11,26 @@ public class SimpleAgedCacheTest {
         assertTrue(cache.isEmpty(), "Cache should be empty on creation");
         assertEquals(0, cache.size(), "Cache size should be 0 on creation");
     }
+    @Test
+    void canPutAndGetEntry() {
+        SimpleAgedCache cache = new SimpleAgedCache();
+        cache.put("foo", 42, 1000);
+        assertEquals(42, cache.get("foo"));
+    }
+
+    @Test
+    void returnsNullForExpiredEntry() throws InterruptedException {
+        SimpleAgedCache cache = new SimpleAgedCache();
+        cache.put("foo", 42, 10); // 10 ms retention
+        Thread.sleep(20);
+        assertNull(cache.get("foo"));
+    }
+
+    @Test
+    void sizeReflectsNumberOfValidEntries() {
+        SimpleAgedCache cache = new SimpleAgedCache();
+        cache.put("foo", 42, 1000);
+        cache.put("bar", 43, 1000);
+        assertEquals(2, cache.size());
+    }
 }
